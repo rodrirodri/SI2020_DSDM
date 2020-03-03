b1d4be95fd42c420fbaf55a892143b0e0674c0b9
rcpadilha <robertocesardasilva@gmail.com>
2020-03-03 20:37:26 -0300

    Aula 03/03/2020

--- HelloWorld/app/src/main/java/br/edu/unisep/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/br/edu/unisep/helloworld/MainActivity.kt
@@ -2,11 +2,24 @@ package br.edu.unisep.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSend.setOnClickListener {
+            send()
+        }
+    }
+
+    private fun send() {
+        val name = editTextName.text.toString()
+        val message = "Olá, $name"
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
