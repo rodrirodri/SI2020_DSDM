0cbc280a04fa9281469412ff6c3561134b32959a
Roberto Padilha <robertocesardasilva@gmail.com>
2020-04-28 20:58:16 -0300

    Aula 28/04

--- CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeFragment.kt
+++ CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeFragment.kt
@@ -2,30 +2,21 @@ package br.edu.unisep.covidreport.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import br.edu.unisep.covidreport.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by lazy {
+        ViewModelProvider(this).get(HomeViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
-    }
+    ) = inflater.inflate(R.layout.fragment_home, container, false)
+
 }
