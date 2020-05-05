9bf223fabba9cf31203890e15014be4b3f3107ca
Roberto Padilha <robertocesardasilva@gmail.com>
2020-05-04 22:48:24 -0300

    Aula 04/05

--- CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeFragment.kt
+++ CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeFragment.kt
@@ -2,10 +2,16 @@ package br.edu.unisep.covidreport.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.edu.unisep.covidreport.R
+import br.edu.unisep.covidreport.domain.dto.TotalDto
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.text.NumberFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
 
@@ -18,5 +24,31 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel.totals.observe(viewLifecycleOwner, Observer { onTotalsResult(it) })
+        homeViewModel.totalsError.observe(viewLifecycleOwner, Observer { onTotalsError() })
+
+        homeViewModel.getTotals()
+    }
+
+    private fun onTotalsResult(totals: TotalDto) {
+        val formatter = NumberFormat.getInstance(Locale("pt", "BR"))
+
+        textViewTotalCases.text = formatter.format(totals.confirmed)
+        textViewCritical.text = formatter.format(totals.critical)
+        textViewRecovered.text = formatter.format(totals.recovered)
+        textViewDeaths.text = formatter.format(totals.deaths)
+
+        viewTotals.visibility = View.VISIBLE
+        progressBarTotals.visibility = View.GONE
+    }
+
+    private fun onTotalsError() {
+        progressBarTotals.visibility = View.GONE
+        textViewErrorTotals.visibility = View.VISIBLE
+    }
 
 }
--- CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeViewModel.kt
+++ CovidReport/app/src/main/java/br/edu/unisep/covidreport/ui/home/HomeViewModel.kt
@@ -1,13 +1,29 @@
 package br.edu.unisep.covidreport.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.edu.unisep.covidreport.domain.dto.TotalDto
+import br.edu.unisep.covidreport.domain.repository.CovidRepository
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val repository = CovidRepository()
+
+    val totals = MutableLiveData<TotalDto>()
+    val totalsError = MutableLiveData<Exception>()
+
+    fun getTotals() {
+        viewModelScope.launch {
+
+            try {
+                val result = repository.getTotals()
+                totals.postValue(result)
+            } catch (e: Exception) {
+                totalsError.postValue(e)
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
 }
